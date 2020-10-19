9bcb817f2937f70d126093dbd6e9c1fc07abdf53
Maximilian Rüdiger Springenberg <springenberg@campus.tu-berlin.de>
2020-10-19 21:16:12 +0200

    documentation

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 """
 This project trains and evaluates autoencoders for image colorization.
 
-* The general autoencoder is defined in :mod:'src.nn'
+* The general autoencoder is defined in :mod:`src.nn`
 * :mod:`src.training` provides a script and methods for training
 * :mod:`src.eval` provides a script and methods for evaluation
 * :mod:`src.data` provides a fast data loader for small data sets
